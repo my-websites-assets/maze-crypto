b2e6b0a8eeebafce4670ef0c848044e28f69d64e
Anthony Njoroge <njorogemainaanthony@gmail.com>
2023-04-29 13:05:44 +0300

    Add a reserve chart to the home page

--- js/dashboard/dashboard-1.js
+++ js/dashboard/dashboard-1.js
@@ -821,6 +821,160 @@
 
 	}
 
+	var TotalReserveChart = function(){
+		var options = {
+		  series: [
+			{
+				name: 'Net Profit',
+				data: [200,300, 200, 250, 200, 240, 180,230,200, 200, 200],
+				/* radius: 30,	 */
+			},
+		],
+			chart: {
+			type: 'area',
+			height: 130,
+			width: 400,
+			toolbar: {
+				show: false,
+			},
+			zoom: {
+				enabled: false
+			},
+			sparkline: {
+				enabled: true
+			}
+
+		},
+
+		colors:['var(--primary)'],
+		dataLabels: {
+		  enabled: false,
+		},
+
+		legend: {
+			show: false,
+		},
+		stroke: {
+		  show: true,
+		  width: 3,
+		  curve:'smooth',
+		  colors:['#DE97DF'],
+		},
+
+		grid: {
+			show:false,
+			borderColor: '#eee',
+			padding: {
+				top: 0,
+				right: 0,
+				bottom: 0,
+				left: 0
+
+			}
+		},
+		states: {
+                normal: {
+                    filter: {
+                        type: 'none',
+                        value: 0
+                    }
+                },
+                hover: {
+                    filter: {
+                        type: 'none',
+                        value: 0
+                    }
+                },
+                active: {
+                    allowMultipleDataPointsSelection: false,
+                    filter: {
+                        type: 'none',
+                        value: 0
+                    }
+                }
+            },
+		xaxis: {
+			categories: ['Jan', 'feb', 'Mar', 'Apr', 'May', 'June', 'July','August', 'Sept','Oct'],
+			axisBorder: {
+				show: false,
+			},
+			axisTicks: {
+				show: false
+			},
+			labels: {
+				show: false,
+				style: {
+					fontSize: '12px',
+				}
+			},
+			crosshairs: {
+				show: false,
+				position: 'front',
+				stroke: {
+					width: 1,
+					dashArray: 3
+				}
+			},
+			tooltip: {
+				enabled: true,
+				formatter: undefined,
+				offsetY: 0,
+				style: {
+					fontSize: '12px',
+				}
+			}
+		},
+		yaxis: {
+			show: false,
+		},
+		fill: {
+		  opacity: 0.9,
+		  colors:'#DE97DF',
+		  type: 'gradient',
+		  gradient: {
+			colorStops:[
+
+				{
+				  offset: 0,
+				  color: '#DE97DF',
+				  opacity: .8
+				},
+				{
+				  offset: 0.6,
+				  color: '#DE97DF',
+				  opacity: .6
+				},
+				{
+				  offset: 100,
+				  color: 'white',
+				  opacity: 0
+				}
+			  ],
+
+			}
+		},
+		/* fill: {
+		  opacity: 0.9,
+		  colors:'#DE97DF'
+		}, */
+		tooltip: {
+			enabled:false,
+			style: {
+				fontSize: '12px',
+			},
+			y: {
+				formatter: function(val) {
+					return "$" + val + " thousands"
+				}
+			}
+		}
+		};
+
+		var chartBar1 = new ApexCharts(document.querySelector("#TotalReserveChart"), options);
+		chartBar1.render();
+
+	}
+
 	/* Function ============ */
 		return {
 			init:function(){
@@ -834,6 +988,7 @@
 				TotaldipositChart();
                 TotalWithdrawalsChart();
                 TotalInvestmentChart();
+				TotalReserveChart();
 			},
 
 			resize:function(){
